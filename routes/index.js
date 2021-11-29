4f2d78c33757c30d55a3e0830cfb8c58db2de0cd
Kaleb Olson <kalebroyolson@gmail.com>
2021-11-29 17:08:14 -0600

    Set up gitignore

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ const router = express.Router()
 // @desc   login/landing page
 // @route  GET /
 router.get('/', (req, res) => {
-    res.render('login')
+    res.render('login', {
+        layout: 'login'
+    })
 })
 // @desc   Dashboard
 // @route  GET /dashboard
